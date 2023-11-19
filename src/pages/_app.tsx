db7cf96acc66b99b5f728569c420d92a12ae3b1b
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-11-20 00:06:49 +0900

    chore: delete unnecessary lines

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Noto_Sans } from 'next/font/google';
+
+const NotoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '800'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={NotoSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
