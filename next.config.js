5cf075c141a895b5da130b25bf0687e23907fe08
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-11-19 23:51:11 +0900

    chores: add dependencies & next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
