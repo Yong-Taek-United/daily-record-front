075ae2074ebcadca517e1d505e9f35760c504b1e
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-12-01 23:58:17 +0900

    modify styles in modal

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,21 +1,24 @@
 import { ReactNode } from 'react';
 import ReactDOM from 'react-dom';
 
-type ModalProps = {
+export interface ModalProps {
   children?: ReactNode;
   onClose: () => void;
   visible: boolean;
-};
+}
 
 export default function Modal({ children, onClose, visible }: ModalProps) {
   if (!visible) return null;
   return ReactDOM.createPortal(
     <>
       <div
+        id="overlay"
         onClick={onClose}
-        className="fixed bg-black top-0 min-h-screen min-w-full opacity-50"
+        className="fixed inset-0 bg-black  opacity-60"
       ></div>
-      <div className="">{children}</div>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        {children}
+      </div>
     </>,
     document.getElementById('portal') as HTMLElement
   );
