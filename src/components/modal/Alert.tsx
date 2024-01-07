133072ba336a8e71970b2d46780367eee365f5b1
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-01-07 23:34:50 +0900

    feat: Add handleAlertConfirmation and handleCloseAlert to Alert component
    - Implement functions to handle confirmation and close actions in the Alert component.
    - Enhance user interaction by providing appropriate callbacks for alert actions.
    - Improve modularity and maintainability of the Alert component.

--- src/components/modal/Alert.tsx
+++ src/components/modal/Alert.tsx
@@ -3,6 +3,8 @@ import SvgX from '../../../public/images/x.svg';
 interface AlertProps extends ModalProps {
   title?: string;
   content: string;
+  handleAlertConfirmation?: () => void;
+  handleCloseAlert?: () => void;
 }
 
 export default function Alert({
@@ -10,14 +12,16 @@ export default function Alert({
   onClose,
   title = '알림',
   content,
+  handleAlertConfirmation,
+  handleCloseAlert,
 }: AlertProps) {
   return (
     <Modal visible={visible} onClose={onClose}>
       <div className={`w-80 h-52 bg-white rounded-lg px-7 py-4 `}>
         <div className="h-6">
           <button
-            onClick={onClose}
-            className="absolute right-3 hover:bg-gray-200 rounded-sm w-5 h-5"
+            onClick={handleCloseAlert ? handleCloseAlert : onClose}
+            className="absolute w-5 h-5 rounded-sm right-3 hover:bg-gray-200"
           >
             <SvgX className="m-auto" width={14} height={14} />
           </button>
@@ -38,8 +42,10 @@ export default function Alert({
           </div>
 
           <button
-            onClick={onClose}
-            className="w-full h-10 rounded-md text-white bg-blue-600 hover:bg-blue-700"
+            onClick={
+              handleAlertConfirmation ? handleAlertConfirmation : onClose
+            }
+            className="w-full h-10 text-white bg-blue-600 rounded-md hover:bg-blue-700"
           >
             확인
           </button>
