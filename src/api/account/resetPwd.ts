a617cdf496f621344287f695316ecbb1192b0ef4
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-01-09 23:33:33 +0900

    feat: Implement password change API

--- src/api/account/resetPwd.ts
+++ src/api/account/resetPwd.ts
@@ -15,3 +15,28 @@ export async function verifyEmailAuthForPwd({ email }: { email: string }) {
     throw error;
   }
 }
+
+export async function changePassword({
+  emailToken,
+  emailLogId,
+  newPassword,
+}: {
+  emailToken: string;
+  emailLogId: number;
+  newPassword: string;
+}) {
+  try {
+    const { status, data } = await instance.patch('/users/password/reset', {
+      emailToken,
+      emailLogId,
+      newPassword,
+    });
+    if (status === 200) {
+      return { data: { data, status } };
+    } else {
+      throw { error: { data, status } };
+    }
+  } catch (error) {
+    throw error;
+  }
+}
