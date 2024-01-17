65ff03c4594faba45498fdb6705ad27ee7facc67
mintaek <mtseo@devmonster.co.kr>
2024-01-17 20:39:32 +0900

    chore: add account manage page

---
+++ src/pages/account/manage.tsx
@@ -0,0 +1,9 @@
+import Navbar from '@/components/UI/Navbar';
+
+export default function AccountUserPage() {
+  return (
+    <main className="bg-gray-50 w-full h-screen">
+      <Navbar />
+    </main>
+  );
+}
