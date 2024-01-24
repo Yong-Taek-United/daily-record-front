b56e9f88a70c0cfa58b56de4532ce2801fb222f0
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-01-24 19:34:08 +0900

    feat: implement token refresh with axios interceptors

--- src/api/authInterceptor.ts
+++ src/api/authInterceptor.ts
@@ -1,12 +1,8 @@
 import { AxiosInstance, AxiosResponse } from 'axios';
 import { Holder } from './holder';
+import { getCookie } from 'cookies-next';
 
-type ReqDataType = {
-  resultCode: number;
-  resultHash: any;
-};
-
-const REFRESH_API = '/login/refresh_token';
+const REFRESH_API = '/auth/refresh';
 
 export const authInterceptors = (instance: AxiosInstance) => {
   let lock = false;
@@ -16,8 +12,19 @@ export const authInterceptors = (instance: AxiosInstance) => {
     if (config.url === REFRESH_API) {
       // 리프래시 토큰 헤더에 담는 로직 짜기
       // 토큰 없으면 바로 세션 만료
+      const refreshToken = getCookie('refreshToken');
+
+      if (refreshToken) {
+        config.headers.Authorization = `Bearer ${refreshToken}`;
+      } else {
+        window.location.href = '/account/login';
+      }
     } else {
-      // 일반 어세스 토큰 헤더에 담는 로직 짜기
+      // 일반 어세스 토큰 헤더에 담는 로직
+      const accessToken = getCookie('accessToken', {});
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
     }
 
     if (lock && config.url !== REFRESH_API) await holder.promise; // accessToken 재발급 요청 중에는 다른 요청을 잠시 hold 함
@@ -36,35 +43,37 @@ export const authInterceptors = (instance: AxiosInstance) => {
         config,
       } = axiosError;
 
-      if (status === 401) {
-        // 토큰 만료 됐을때 리프래쉬 로직 작성
-        if (error?.message == 'AccessTokenExpiredError') {
-          try {
-            if (!lock) {
-              lock = true;
-              holder.hold();
-              try {
-                const res = instance(REFRESH_API);
-                // res.data.status === 200 이면 토큰 정보 담기 (쿠키일 경우 서버에서 담아 줌)
+      if (status && error.path === REFRESH_API) {
+        // refreshToken도 만료
+        window.location.href = '/account/login';
+      } else if (status === 401) {
+        // accessToken 토큰 만료 됐을때 리프래쉬 실행
+        try {
+          if (!lock) {
+            // hold!
+            lock = true;
+            holder.hold();
 
-                setTimeout(() => {
-                  holder.resolve();
-                  lock = false;
-                }, 500);
-              } catch (error) {
-                lock = false;
-                holder.reject();
-                throw new Error('Refresh token renewal failed');
-              }
-            } else await holder.promise; // 이미 accessToken 재발급 요청을 했다면  hold
+            try {
+              // refresh!
+              const res = await instance.post(REFRESH_API);
+              console.log(res);
+              holder.resolve();
+              lock = false;
+            } catch (error) {
+              lock = false;
+              holder.reject();
+              throw new Error('Refresh token renewal failed');
+            }
+          } else await holder.promise; // 이미 accessToken 재발급 요청을 했다면  hold
 
-            return instance(config); // 기존 요청을 재요청
-          } catch (error) {
-            console.log('interceptor error ', error);
-          }
+          return instance(config); // 기존 요청을 재요청
+        } catch (error) {
+          console.log('interceptor error ', error);
         }
+      } else {
+        return Promise.resolve(config);
       }
-
       return Promise.reject(axiosError);
     }
   );
