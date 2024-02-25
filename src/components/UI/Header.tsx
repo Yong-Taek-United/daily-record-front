50afc018fea1553d9cea4d190ed6868ad1178357
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-25 23:09:16 +0900

    feat: update Header with profile image and improved nav paths
    - added ProfileImg for user profile display.
    - updated nav paths for account management and settings.
    - implemented local storage data fetch for user info.
    - added logout option directly in nav menu.

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,6 +1,9 @@
 import { logout } from '@/api/account/auth';
+import { SaveUserInfoTypes } from '@/types/user';
+import { getDataFromLocalStorage } from '@/utils/utils';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import ProfileImg from './ProfileImg';
 
 type MenuListType = {
   name: string;
@@ -9,14 +12,15 @@ type MenuListType = {
 };
 
 const MENU_LIST: MenuListType[] = [
-  { name: '내 프로필', path: '/account/info' },
-  { name: '설정', path: '/account/manage' },
-  { name: '로그아웃', method: () => null },
+  { name: '내 프로필', path: '/account/manage/info' },
+  { name: '설정', path: '/account/manage/setting' },
 ];
 
 export default function Header() {
   const router = useRouter();
   const [openMenu, setOpenMenu] = useState(false);
+  const user: SaveUserInfoTypes =
+    typeof window !== 'undefined' && getDataFromLocalStorage('user');
 
   const handleLogout = async () => {
     try {
@@ -26,6 +30,7 @@ export default function Header() {
       }
     } catch (error) {}
   };
+
   return (
     <div className="w-full h-16 bg-white flex justify-between p-4">
       <div className="my-auto">
@@ -37,16 +42,20 @@ export default function Header() {
         </button>
       </div>
       <div className="relative flex my-auto">
-        <button
-          onClick={() => setOpenMenu(!openMenu)}
-          className="w-8 h-8 bg-slate-500 rounded-full"
-        ></button>
+        <div>
+          <ProfileImg
+            onClick={() => setOpenMenu(!openMenu)}
+            width={32}
+            height={32}
+          />
+        </div>
+
         {openMenu ? (
           <div
             className={`absolute z-50 top-10 right-0 w-auto border rounded-md bg-white`}
           >
             <div className="w-40 p-2 border-b">
-              <button className="font-semibold">앵가리</button>
+              <button className="font-semibold">{user?.nickname}</button>
             </div>
             {MENU_LIST.map((menu, i) => (
               <div key={menu.name} className="p-2">
@@ -62,6 +71,14 @@ export default function Header() {
                 </button>
               </div>
             ))}
+            <div className="p-2">
+              <button
+                onClick={handleLogout}
+                className="w-full text-left font-medium text-[#828282] hover:text-black duration-500"
+              >
+                로그아웃
+              </button>
+            </div>
           </div>
         ) : null}
       </div>
