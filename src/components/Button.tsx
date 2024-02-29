303f3e6b7fe757ae989198a11e8b84771fb25734
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-29 23:57:51 +0900

    refactor: apply tailwind-merge to Button component styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import { ButtonHTMLAttributes, CSSProperties, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface ButtonTypes extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
@@ -8,11 +9,12 @@ export default function Button({ children, style, ...props }: ButtonTypes) {
   return (
     <button
       {...props}
-      className={
+      className={twMerge(
         props.disabled
           ? `bg-gray-400 cursor-not-allowed text-white font-bold text-lg rounded-md w-96 h-10`
-          : `bg-black text-white font-bold text-lg rounded-md w-96 h-10`
-      }
+          : `bg-black text-white font-bold text-lg rounded-md w-96 h-10`,
+        `${props.className}`
+      )}
       style={{ ...style }}
     >
       {children}
