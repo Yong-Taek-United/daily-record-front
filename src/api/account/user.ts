66559d48421cf9b38b20d99394959f38847e7824
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-10 22:24:56 +0900

    feat: add update user introduction api

--- src/api/account/user.ts
+++ src/api/account/user.ts
@@ -8,3 +8,8 @@ export function getUserInfo(): Promise<
 > {
   return instance.get('/users/info');
 }
+
+/** 회원 프로필 한 줄 소개 변경 */
+export function updateUserIntroduce({ introduce }: { introduce: string }) {
+  return instance.patch('/users/profile', { introduce });
+}
