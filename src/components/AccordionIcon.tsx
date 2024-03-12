e4ec7067adc634daecc4a63f4889d83a299bbadf
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-12 22:05:58 +0900

    add AccordionIcon component

---
+++ src/components/AccordionIcon.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface PropsType extends React.HTMLProps<HTMLDivElement> {
+  isOpen: boolean;
+}
+
+const AccordionIcon = ({ isOpen, ...props }: PropsType) => {
+  return (
+    <div className={`relative w-6 h-6 ${twMerge(props.className)}`}>
+      <div
+        className={`absolute inset-0 w-full h-full transform transition-transform duration-300 ${
+          isOpen ? '' : 'rotate-180'
+        }`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className="w-full h-full text-gray-400"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 15l7-7 7 7"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default AccordionIcon;
