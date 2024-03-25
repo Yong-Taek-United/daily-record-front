e9753aa822f52dfe3945aa729d97d9432608b7b7
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-25 23:48:47 +0900

    feat: dash-board page with fetch project list data using SSR

---
+++ src/pages/projects/dash-board.tsx
@@ -0,0 +1,104 @@
+import {
+  GetProjectsParams,
+  GetProjectsRes,
+  getProjects,
+} from '@/api/projects/projects';
+import { setContext } from '@/api/authInterceptorWithSSR';
+import Container from '@/components/UI/Container';
+import Header from '@/components/UI/Header';
+import { ProjectListItem } from '@/components/projects/ProjectListItem';
+import { ProjectFilterType } from '@/types/projects';
+import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+type ParamsType = {
+  page?: string;
+  filter?: ProjectFilterType;
+};
+
+const LIST_TAKE = 10;
+const FILTER_LIST: { name: string; value: ProjectFilterType }[] = [
+  { name: '진행 중인 프로젝트', value: 'ONGOING' },
+  { name: '예정된 프로젝트', value: 'SCHEDULED' },
+  { name: '지난 프로젝트', value: 'COMPLETED' },
+];
+
+export default function DashBoardPage(props: {
+  initialListData: GetProjectsRes[];
+  initialListCount: number;
+  initialPage: number;
+  initialFilter: ProjectFilterType;
+}) {
+  const [listData, setListData] = useState<GetProjectsRes[]>(
+    props.initialListData || []
+  );
+  const [listCount, setListCount] = useState(props.initialListCount || 0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const [currentPage, setCurrentPage] = useState<number>(
+    props.initialPage || 1
+  );
+  const [currentFilter, setCurrentFilter] = useState<ProjectFilterType>(
+    props.initialFilter || 'ONGOING'
+  );
+
+  return (
+    <div>
+      <Header />
+      <Container>
+        <div className="my-4">
+          <h1>대시보드</h1>
+        </div>
+
+        {listData.map((project) => (
+          <ProjectListItem key={project.id} item={project} />
+        ))}
+      </Container>
+    </div>
+  );
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<ParamsType>
+) {
+  const { page = 1, filter = 'ONGOING' } = context.query as ParamsType;
+  const listSkip = (Number(page) - 1) * LIST_TAKE;
+  setContext(context);
+  try {
+    const { data } = await getProjects({
+      listTake: LIST_TAKE,
+      listSkip,
+      projectStatus: filter,
+    });
+
+    console.log(data);
+
+    if (data?.data?.length) {
+      return {
+        props: {
+          initialListData: data.data,
+          initialListCount: data.data.length,
+          initialPage: Number(page),
+          initialFilter: filter,
+        },
+      };
+    } else {
+      return {
+        props: {
+          initialListData: [],
+          initialListCount: 0,
+          initialPage: Number(page),
+          initialFilter: filter,
+        },
+      };
+    }
+  } catch (err) {
+    return {
+      props: {
+        error: '프로젝트 목록을 불러오는데 실패했습니다.',
+      },
+    };
+  }
+}
