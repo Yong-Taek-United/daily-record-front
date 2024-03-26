65f239c4596874b4573bf1b7ebbf27672f8dd6dd
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-26 21:06:06 +0900

    update image imports in utils imgaes

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,7 @@
-import SvgCheck from '@images/authCheckImg.svg';
-import SvgUpdatePencil from '@images/updateButton.svg';
-export { SvgCheck, SvgUpdatePencil };
+import SvgCheck from '@images/auth-check-img.svg';
+import SvgUpdatePencil from '@images/update-button.svg';
+import SvgHat from '@images/hat.svg';
+import SvgDotedCheck from '@images/doted-check.svg';
+import SvgPlus from '@images/plus.svg';
+
+export { SvgCheck, SvgUpdatePencil, SvgHat, SvgDotedCheck, SvgPlus };
