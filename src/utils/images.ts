73da1228491021c47882495ca6f4f868a1be5643
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-27 23:43:31 +0900

    add new SVG images

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -3,5 +3,15 @@ import SvgUpdatePencil from '@images/update-button.svg';
 import SvgHat from '@images/hat.svg';
 import SvgDotedCheck from '@images/doted-check.svg';
 import SvgPlus from '@images/plus.svg';
+import SvgCalendar from '@images/calendar.svg';
+import SvgFilledDownArrow from '@images/filled-down-arrow.svg';
 
-export { SvgCheck, SvgUpdatePencil, SvgHat, SvgDotedCheck, SvgPlus };
+export {
+  SvgCheck,
+  SvgUpdatePencil,
+  SvgHat,
+  SvgDotedCheck,
+  SvgPlus,
+  SvgCalendar,
+  SvgFilledDownArrow,
+};
