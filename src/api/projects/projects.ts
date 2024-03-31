63adec1e74b43f6d13e67a8246e971e05bd06cf8
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-31 21:26:19 +0900

    feat: API createProject and updateProject

--- src/api/projects/projects.ts
+++ src/api/projects/projects.ts
@@ -21,7 +21,13 @@ export type GetProjectsRes = {
   updatedAt: Date;
   deletedAt: string;
 };
-
+export type CreateProjectReq = {
+  title: string;
+  description: string;
+  startedAt: string;
+  finishedAt: string;
+  isActive: boolean;
+};
 export async function getProjects({
   listSkip,
   listTake,
@@ -38,4 +44,27 @@ export async function getTasks({
   projectId: number;
 }): Promise<AxiosResponse<{ data: TasksTypes[] }>> {
   return await instanceSSR.get(`/tasks/list/${projectId}`);
+}
+
+export async function createProject(
+  data: CreateProjectReq
+): Promise<AxiosResponse<{ data: any }, any>> {
+  return await instanceSSR.post('/projects', {
+    ...data,
+  });
+}
+
+export async function getProjectDetail(
+  projectId: number
+): Promise<AxiosResponse<{ data: any }>> {
+  return await instanceSSR.get(`/projects/${projectId}`);
+}
+
+export async function updateProject(
+  data: CreateProjectReq,
+  projectId: number
+): Promise<AxiosResponse<{ data: any }>> {
+  return await instanceSSR.put(`/projects/${projectId}`, {
+    ...data,
+  });
 }
