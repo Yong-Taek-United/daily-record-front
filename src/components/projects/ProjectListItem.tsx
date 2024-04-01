7b68e7316ee20f597a3fcdcf945dc3b57865f0a5
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-01 23:27:12 +0900

    feat: add task creation and update functionality to ProjectListItem component

--- src/components/projects/ProjectListItem.tsx
+++ src/components/projects/ProjectListItem.tsx
@@ -6,17 +6,21 @@ import AccordionIcon from '../AccordionIcon';
 import { TasksTypes } from '@/types/projects';
 import TaskListItem from './task/TaskListItem';
 import { useRouter } from 'next/router';
+import TaskEditForm from './task/TaskEditForm';
 
 type PropsType = {
   item: GetProjectsRes;
+  isLastItem: boolean;
 };
-export function ProjectListItem({ item }: PropsType) {
+export function ProjectListItem({ item, isLastItem }: PropsType) {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   const [didFetch, setDidFetch] = useState(false);
   const [tasksList, setTaskList] = useState<TasksTypes[]>([]);
+  const [createTaskForm, setCreateTaskForm] = useState<boolean>(false);
+  const [isUpdatingTaskId, setIsUpdatingTaskId] = useState<number | null>(null);
 
   const getTasksList = async () => {
     try {
@@ -42,8 +46,14 @@ export function ProjectListItem({ item }: PropsType) {
   };
 
   return (
-    <div className={`relative overflow-hidden border-b p-4 border-gray-400`}>
-      <div className="">
+    <div className={`relative overflow-hidden `}>
+      <div
+        className={`p-4 ${
+          isLastItem
+            ? 'border-t border-b border-gray-500'
+            : 'border-t border-b-0 border-gray-500'
+        } `}
+      >
         <div className="flex flex-row justify-between w-full align-middle h-28">
           {/* 좌측 */}
           <button
@@ -84,22 +94,55 @@ export function ProjectListItem({ item }: PropsType) {
       </div>
       {isOpen ? (
         <div className={``}>
-          <div className="flex justify-end">
-            <button className="flex items-center h-auto">
-              <div className="mr-2 ">
-                <SvgPlus />
-              </div>
-              <p className="text-gray-800 underline">과제 생성하기</p>
-            </button>
+          <div className="flex justify-end py-4">
+            {!createTaskForm && (
+              <button
+                onClick={() => setCreateTaskForm(true)}
+                className="flex items-center h-auto"
+              >
+                <div className="mr-2 ">
+                  <SvgPlus />
+                </div>
+                <p className="text-gray-800 underline">과제 생성하기</p>
+              </button>
+            )}
+          </div>
+          <div>
+            {createTaskForm && (
+              <TaskEditForm
+                projectId={item.id}
+                isUpdate={false}
+                taskData={null}
+                onCancel={() => setCreateTaskForm(false)}
+                onSuccessful={() => getTasksList()}
+              />
+            )}
           </div>
           {tasksList?.length ? (
-            tasksList.map((task, i) => (
-              <TaskListItem
-                key={i}
-                task={task}
-                isLastTask={tasksList.length === i + 1}
-              />
-            ))
+            tasksList.map((task, i) =>
+              isUpdatingTaskId === task.id ? (
+                <TaskEditForm
+                  key={i}
+                  projectId={item.id}
+                  isUpdate={true}
+                  taskData={task}
+                  onCancel={() => setIsUpdatingTaskId(null)}
+                  onSuccessful={() => getTasksList()}
+                />
+              ) : (
+                <TaskListItem
+                  key={i}
+                  task={task}
+                  isLastTask={tasksList.length === i + 1}
+                  onDeleteTask={() => {
+                    let temp = [...tasksList];
+                    temp.splice(i, 1);
+                    setTaskList(temp);
+                  }}
+                  onUpdateTask={(taskId) => setIsUpdatingTaskId(taskId)}
+                />
+              )
+            )
           ) : (
             <div className="w-full py-5 text-base font-normal text-gray-500 ">
               <p className="mx-auto text-center">과제가 없습니다.</p>
@@ -107,7 +150,6 @@ export function ProjectListItem({ item }: PropsType) {
           )}
         </div>
       ) : null}
-      {/* 과제 */}
     </div>
   );
 }
