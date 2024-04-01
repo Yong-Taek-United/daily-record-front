808e10b93a9a57d21e4f5b14b2d42d195570b8d5
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-01 23:28:49 +0900

    update font in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Noto_Sans } from 'next/font/google';
+import { Noto_Sans, Inter } from 'next/font/google';
 
 const NotoSans = Noto_Sans({
   subsets: ['latin'],
   weight: ['400', '500', '800'],
 });
 
+const InterFont = Inter({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '600', '800'],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={NotoSans.className}>
+    <main className={InterFont.className}>
       <Component {...pageProps} />
     </main>
   );
