9d0d045d95e24fbee973b8a958a5452883ac934a
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-07 11:54:02 +0900

    style: add event styling in Calendar

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,25 +1,42 @@
 import moment from 'moment';
 import { Calendar, CalendarProps, momentLocalizer } from 'react-big-calendar';
-import Toolbar from './Toolbar';
 import 'moment/locale/ko';
+import { CSSProperties } from 'react';
+import { CalendarEvent } from '@/pages/main';
 
 moment.locale('ko');
 const localizer = momentLocalizer(moment);
 
 export default function MyCalendar(props: Omit<CalendarProps, 'localizer'>) {
+  // 이벤트 스타일 변경
+  const customEventPropGetter = (event: CalendarEvent) => {
+    let newStyle: CSSProperties = {
+      backgroundColor: 'lightblue',
+      color: 'black',
+      border: 'none',
+    };
+
+    if (event.resource?.task?.color?.colorCode) {
+      newStyle.backgroundColor = `#${event.resource.task.color.colorCode}`;
+    }
+
+    return {
+      style: newStyle,
+    };
+  };
+
   return (
     <Calendar
-      components={{
-        toolbar: Toolbar,
-      }}
+      {...props}
       view={'month'}
       localizer={localizer}
-      events={props.events}
       startAccessor="start"
       endAccessor="end"
       messages={{
         showMore: (count) => `+${count}`,
       }}
+      eventPropGetter={(event) => customEventPropGetter(event as CalendarEvent)}
+      onView={(view) => null}
     />
   );
 }
