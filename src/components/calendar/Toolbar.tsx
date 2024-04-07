3f0a93c37d775bcca2365439ccd0ed84ab04608b
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-07 11:52:29 +0900

    feat: add handle navigate function to Toolbar'

--- src/components/calendar/Toolbar.tsx
+++ src/components/calendar/Toolbar.tsx
@@ -1,7 +1,27 @@
 import { SvgRigthArrow } from '@/utils/images';
+
+import { useRouter } from 'next/router';
 import { ToolbarProps } from 'react-big-calendar';
 
 export default function Toolbar({ date, onNavigate }: ToolbarProps) {
+  const router = useRouter();
+
+  const handleNavigate = (action: 'PREV' | 'NEXT') => {
+    onNavigate(action);
+
+    const newDate = new Date(date);
+    if (action === 'PREV') {
+      newDate.setMonth(date.getMonth() - 1);
+    } else if (action === 'NEXT') {
+      newDate.setMonth(date.getMonth() + 1);
+    }
+
+    router.push({
+      pathname: router.pathname,
+      query: { year: newDate.getFullYear(), month: newDate.getMonth() + 1 },
+    });
+  };
+
   return (
     <div className="flex justify-between py-2 px-4 sm:py-4 sm:px-8">
       <div>
@@ -10,10 +30,10 @@ export default function Toolbar({ date, onNavigate }: ToolbarProps) {
         }월`}</p>
       </div>
       <div className="flex items-center">
-        <button onClick={() => onNavigate('PREV')} className="px-3">
+        <button onClick={() => handleNavigate('PREV')} className="px-3">
           <SvgRigthArrow className="rotate-180" />
         </button>
-        <button onClick={() => onNavigate('NEXT')} className="px-3">
+        <button onClick={() => handleNavigate('NEXT')} className="px-3">
           <SvgRigthArrow />
         </button>
       </div>
