173adf4843311d8d3ca28648bf9504dceb928209
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-10 21:45:12 +0900

    add ActivitiesList and MobileActivitiesList components in main page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,6 +3,9 @@ import { getCalendarData } from '@/api/calendar/calendar';
 import { ActivityType, CalendarDataType } from '@/types/calendar';
 import { Event as RCBEvent } from 'react-big-calendar';
 import { GetServerSidePropsContext } from 'next';
+import { Children, cloneElement, useState } from 'react';
+import ActivitiesList from '@/components/calendar/ActivitiesList';
+import MobileActivitiesList from '@/components/calendar/MobileActivitiesList';
 import Header from '@/components/UI/Header';
 import Toolbar from '@/components/calendar/Toolbar';
 import dynamic from 'next/dynamic';
@@ -35,9 +38,9 @@ export default function Main({
     let events: CalendarEvent[] = CalendarData.flatMap((data) =>
       data.activities.map((activity) => ({
         id: activity.id,
-        title: activity.title,
-        start: new Date(activity.actedDate + 'T' + activity.actedTime),
-        end: new Date(activity.actedDate + 'T' + activity.actedTime),
+        title: activity.task?.title,
+        start: new Date(activity.actedDate),
+        end: new Date(activity.actedDate),
         resource: activity,
       }))
     );
@@ -45,20 +48,67 @@ export default function Main({
     return events;
   };
 
+  const [selectedDate, setSelectedDate] = useState<Date>();
+  const [mobileList, setMobileList] = useState(false);
+
+  const handleSelect = (date: Date) => {
+    setSelectedDate(date);
+    setMobileList(true);
+  };
+
+  function formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-based, so we add 1
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
+
   return (
     <main className="w-full h-screen bg-gray-50">
       <Header />
-
-      <div className="w-full lg:flex lg:justify-between h-2/3 sm:h-4/5  p-3 mt-4">
-        <div className="w-full h-full bg-white">
+      <MobileActivitiesList
+        filteredActivities={activities.filter(
+          (activity) =>
+            activity.actedDate === formatDate(selectedDate || new Date())
+        )}
+        selectedDate={selectedDate || new Date()}
+        isVisible={mobileList}
+        onClose={() => setMobileList(false)}
+      />
+      {mobileList && (
+        <div
+          className="fixed inset-0 top-0 z-20 bg-black bg-opacity-25 md:hidden backdrop-blur-sm"
+          onClick={() => setMobileList(false)}
+        ></div>
+      )}
+      <div className="mx-auto mt-4 max-w-7xl md:flex lg:justify-between h-4/5">
+        <div className="w-full h-full p-3 bg-white rounded-md">
           <MyCalendar
-            components={{ toolbar: Toolbar }}
+            components={{
+              toolbar: Toolbar,
+            }}
             defaultDate={new Date(`${year}-${month}`)}
             events={formatCalendarData(activities)}
+            onSelectEvent={({ start, resource }) => {
+              handleSelect(start as Date);
+            }}
+            onSelectSlot={({ start }) => {
+              handleSelect(start);
+            }}
+            onDrillDown={(date) => handleSelect(date)}
+            selectable={true}
+            longPressThreshold={10}
           />
         </div>
 
-        {/* <div className="hidden lg:block bg-red-200 lg:ml-4 w-32">gd</div> */}
+        <ActivitiesList
+          filteredActivities={activities.filter(
+            (activity) =>
+              activity.actedDate === formatDate(selectedDate || new Date())
+          )}
+          selectedDate={selectedDate || new Date()}
+        />
       </div>
     </main>
   );
