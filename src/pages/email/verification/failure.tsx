44a2b1b90119e0dc6439e59ccd075c2bec9e81f2
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-13 15:02:45 +0900

    refactor: email verification pages

--- src/pages/email/verification/failure.tsx
+++ src/pages/email/verification/failure.tsx
@@ -1,8 +1,17 @@
-import { useEffect } from 'react';
-
 export default function EmailFailure() {
-  useEffect(() => {
-    alert('이메일 인증 실패하였습니다.');
-  }, []);
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="h-screen max-w-xl mx-auto">
+        <div className="w-full h-full pt-20 bg-white rounded-lg">
+          <p className="m-auto text-base font-bold text-center sm:text-lg">
+            인증이 만료되었거나 잘못된 요청입니다.
+          </p>
+          <p className="m-auto text-base font-bold text-center sm:text-lg">
+            인증을 다시 시도해 주세요.
+          </p>
+          <p className="m-auto text-center">현재 페이지는 닫아 주세요.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/email/verification/success.tsx
+++ src/pages/email/verification/success.tsx
@@ -1,8 +1,14 @@
-import { useEffect } from 'react';
-
 export default function EmailSuccess() {
-  useEffect(() => {
-    alert('이메일 인증되었습니다.');
-  }, []);
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="h-screen max-w-xl mx-auto">
+        <div className="w-full h-full pt-20 bg-white rounded-lg">
+          <p className="m-auto text-base font-bold text-center sm:text-lg">
+            이메일 인증이 완료되었습니다.
+          </p>
+          <p className="m-auto text-center">현재 페이지는 닫아 주세요.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
